202db27b8cdcf6b1f7a214fb1b55a75bc1210ea7
marciomess <marciosoares@tecnico.ulisboa.pt>
2021-01-04 18:26:50 +0000

    updated TEX

--- document/document.mk
+++ document/document.mk
@@ -14,7 +14,7 @@ pb.pdf: pb.tex figures fpga_res
 	$(EXPORT_LIST) pdflatex '\def\TEX{$(TEX)}\def\XILINX{$(XILINX)}\def\INTEL{$(INTEL)}\input{$<}'
 	evince $@ &
 
-ug.pdf: $(SRC) figures fpga_res $(CORE_NAME)_version.txt
+ug.pdf: $(SRC) figures fpga_res version.txt
 	git rev-parse --short HEAD > shortHash.txt
 	$(EXPORT_LIST) pdflatex '\def\TEX{$(TEX)}\def\XILINX{$(XILINX)}\def\INTEL{$(INTEL)}\input{$<}'
 	$(EXPORT_LIST) pdflatex '\def\TEX{$(TEX)}\def\XILINX{$(XILINX)}\def\INTEL{$(INTEL)}\input{$<}'
@@ -26,31 +26,19 @@ figures:
 	make -C ../figures
 
 fpga_res:
-	echo $(IS_CORE)
 ifeq ($(XILINX),1)
-ifeq ($(IS_CORE),1)
-	cp $($(CORE_NAME)_HW_DIR)/fpga/vivado/XCKU/vivado.log .
-else
-	cp $(HW_DIR)/fpga/AES-KU040-DB-G/vivado.log .
-endif
+	cp $($(CORE_NAME)_HW_DIR)/fpga/vivado/$(FPGA_FAMILY)/vivado.log .
 endif
 ifeq ($(INTEL),1)
-ifeq ($(IS_CORE),1)
 	cp $($(CORE_NAME)_HW_DIR)/fpga/quartus/CYCLONEV-GT/quartus.log .
-else
-	cp $(HW_DIR)/fpga/CYCLONEV-GT-DK/top_system.fit.summary quartus.log
-endif
 endif
 	$(EXPORT_LIST) $(TEX_SW_DIR)/fpga2tex.sh
 
 texclean:
-	@rm -f *~ *.aux *.out *.log *.summary 
+	@rm -f *~ *.aux *.out *.log *.summary *_results*
 	@rm -f *.lof *.toc *.fdb_latexmk  ug.fls  *.lot *.txt
 	make -C ../figures clean
 	@rm -f $(TEX_SRC) $(addprefix ../figures/, $(TEX_FIG))
-
-resultsclean:
-	@rm *_results*
 
 pdfclean: clean
 	@rm -f *.pdf
